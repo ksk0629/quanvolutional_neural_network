2e3331713c57d4e80f458210eb351f4a006f75a7
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2024-08-23 01:29:08 +0100

    #7 Refactor codes.

--- src/classical_convolutional_neural_network.py
+++ src/classical_convolutional_neural_network.py
@@ -5,8 +5,15 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class ClassicalConvolutionalNeuralNetwork(nn.Module):
-    def __init__(self, input_dimension: tuple[int, int, int], num_classes: int):
+class ClassicalCNN(nn.Module):
+    """Classical CNN class."""
+
+    def __init__(self, in_dim: tuple[int, int, int], num_classes: int):
+        """Initialise this CNN.
+
+        :param tuple[int, int, int] in_dim: input data dimension formed as [channels, height, width]
+        :param int num_classes: number of clssses to classify
+        """
         super().__init__()
         self.kernel_size = 5
         self.stride = 1
@@ -19,7 +26,7 @@ class ClassicalConvolutionalNeuralNetwork(nn.Module):
 
         self.num_filter1 = 50
         self.conv1 = nn.Conv2d(
-            in_channels=input_dimension[0],
+            in_channels=in_dim[0],
             out_channels=self.num_filter1,
             kernel_size=self.kernel_size,
             stride=self.stride,
@@ -27,7 +34,12 @@ class ClassicalConvolutionalNeuralNetwork(nn.Module):
             dilation=self.dilation,
         )
         self.conv1_output_shape = self.calc_output_shape(
-            input_dimension[1], input_dimension[2], self.kernel_size, self.stride
+            in_dim[1],
+            in_dim[2],
+            self.kernel_size,
+            self.stride,
+            self.padding,
+            self.dilation,
         )
         self.pool1 = nn.MaxPool2d(kernel_size=self.pool_size, stride=self.pool_size)
         self.pool1_output_shape = self.calc_output_shape(
@@ -35,6 +47,8 @@ class ClassicalConvolutionalNeuralNetwork(nn.Module):
             self.conv1_output_shape[1],
             self.pool_size,
             self.pool_size,
+            self.padding,
+            self.dilation,
         )
 
         self.num_filter2 = 64
@@ -51,6 +65,8 @@ class ClassicalConvolutionalNeuralNetwork(nn.Module):
             self.pool1_output_shape[1],
             self.kernel_size,
             self.stride,
+            self.padding,
+            self.dilation,
         )
         self.pool2 = nn.MaxPool2d(kernel_size=self.pool_size, stride=self.pool_size)
         self.pool2_output_shape = self.calc_output_shape(
@@ -58,6 +74,8 @@ class ClassicalConvolutionalNeuralNetwork(nn.Module):
             self.conv2_output_shape[1],
             self.pool_size,
             self.pool_size,
+            self.padding,
+            self.dilation,
         )
 
         self.fc1_input_size = (
@@ -75,21 +93,36 @@ class ClassicalConvolutionalNeuralNetwork(nn.Module):
         )
 
     def calc_output_shape(
-        self, input_height: int, input_width: int, kernel_size: int, stride: int
+        self,
+        in_height: int,
+        in_width: int,
+        kernel_size: int,
+        stride: int,
+        padding: int,
+        dilation: int,
     ) -> tuple[int, int]:
+        """Calculate an output shape.
+
+        :param int in_height: input height
+        :param int in_width: input width
+        :param int kernel_size: kernel size
+        :param int stride: stride
+        :return tuple[int, int]: output shape
+        """
         output_height = math.floor(
-            (input_height + 2 * self.padding - self.dilation * (kernel_size - 1) - 1)
-            / stride
-            + 1
+            (in_height + 2 * padding - dilation * (kernel_size - 1) - 1) / stride + 1
         )
         output_width = math.floor(
-            (input_width + 2 * self.padding - self.dilation * (kernel_size - 1) - 1)
-            / stride
-            + 1
+            (in_width + 2 * padding - dilation * (kernel_size - 1) - 1) / stride + 1
         )
         return (output_height, output_width)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward data.
+
+        :param torch.Tensor x: input data
+        :return torch.Tensor: processed data
+        """
         # The first convolutional and pooling layers result.
         x = self.conv1(x)
         x = self.relu(x)
@@ -111,5 +144,10 @@ class ClassicalConvolutionalNeuralNetwork(nn.Module):
         return self.softmax(x, dim=1)
 
     def classify(self, x: torch.Tensor) -> torch.Tensor:
+        """Classify data.
+
+        :param torch.Tensor x: _description_
+        :return torch.Tensor: _description_
+        """
         probabilities = self.forward(x)
         return torch.argmax(probabilities, dim=1)

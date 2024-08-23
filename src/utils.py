13fed4c54f6d7270246cb027c03fd2e92d8386d8
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2024-08-23 15:20:00 +0100

    #11 Create utils and fix_seed function

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+import random
+
+import numpy as np
+import torch
+
+
+def fix_seed(seed: int):
+    """Fix random seed to have reproducibility.
+
+    :param int seed: seed
+    """
+    random.seed(seed)
+
+    np.random.seed(seed)
+
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False

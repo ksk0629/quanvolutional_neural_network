4665bbf6fc2436517605072f21af4a9130c9a492
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2024-08-23 15:16:50 +0100

    Fix bug

--- src/quanv_nn.py
+++ src/quanv_nn.py
@@ -21,12 +21,13 @@ class QuanvNN:
         :param int quanv_num_filters: number of quanvolutional filters
         :param str | None quanv_padding_mode: padding mode (see the document of torch.nn.functional.pad), defaults to "constant"
         """
-        self.classical_cnn = ClassicalCNN(in_dim=in_dim, num_classes=num_classes)
         self.quanv_layer = QuanvLayer(
             kernel_size=quanv_kernel_size,
             num_filters=quanv_num_filters,
             padding_mode=quanv_padding_mode,
         )
+        new_in_dim = (quanv_num_filters, in_dim[1], in_dim[2])
+        self.classical_cnn = ClassicalCNN(in_dim=new_in_dim, num_classes=num_classes)
 
     def __call__(self, x: torch.Tensor, shots: int) -> torch.Tensor:
         """forward a batch data.
